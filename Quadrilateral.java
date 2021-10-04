8e5121598bf811a9fd08af5e8f8ec03a18925257
Jack Meng <jackmeng0814@gmail.com>
2021-10-04 13:38:16 -0400

    Update Quadrilateral.java

--- Quadrilateral.java
+++ Quadrilateral.java
@@ -6,7 +6,16 @@ public class Quadrilateral {
    this.c = c;
    this.d = d;
  }
-  
+ public boolean isTrapezoid() {
+  return a.slope(b) == c.slope(d) ? true : false; 
+ }
+ public boolean isParallelogram() {
+  return a.slope(b) == c.slope(d) && a.slope(d) == c.slope(b) ? true : false; 
+ }
+ public boolean isSquare() {
+  return a.distance(b) == c.distance(d) ? true : false;
+ }
+ 
  public static boolean approx (double a, double b) {
   return (a - b) < 0.000001 ? true : false;
  }
