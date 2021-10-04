59f1e392eef455302d84058f32afcef9b0e5b518
exoad <exoad@users.noreply.github.com>
2021-10-04 18:15:11 +0000

    Prettified Code!

--- Quadrilateral.java
+++ Quadrilateral.java
@@ -3,26 +3,30 @@
  * Purpose: This is a class that uses the fffffffffffffffffffffffffffffffffffffffffffffffff
  */
 public class Quadrilateral {
- private Point p;
- public Quadrilateral (Point a, Point b, Point c, Point d){
-   p = new Point[4];
-  p[1] = a;
-  p[2] = b;
-  p[3] = c;
-  p[4] = d;
- }
- public boolean isTrapezoid() {
-  return a.slope(b) == c.slope(d) ? true : false; 
- }
- public boolean isParallelogram() {
-  return a.slope(b) == c.slope(d) && a.slope(d) == c.slope(b) ? true : false; 
- }
- public boolean isSquare() {
-  return a.distance(b) == c.distance(d) ? true : false;
- }
- 
- public static boolean approx (double a, double b) {
-  return (a - b) < 0.000001 ? true : false;
- }
-  
+
+  private Point p;
+
+  public Quadrilateral(Point a, Point b, Point c, Point d) {
+    p = new Point[4];
+    p[1] = a;
+    p[2] = b;
+    p[3] = c;
+    p[4] = d;
+  }
+
+  public boolean isTrapezoid() {
+    return a.slope(b) == c.slope(d) ? true : false;
+  }
+
+  public boolean isParallelogram() {
+    return a.slope(b) == c.slope(d) && a.slope(d) == c.slope(b) ? true : false;
+  }
+
+  public boolean isSquare() {
+    return a.distance(b) == c.distance(d) ? true : false;
+  }
+
+  public static boolean approx(double a, double b) {
+    return (a - b) < 0.000001 ? true : false;
+  }
 }
