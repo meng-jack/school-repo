5003a8e3858b031c47c7ae2e1d42b42a369bbb36
Jack Meng <jackmeng0814@gmail.com>
2021-10-04 13:32:31 -0400

    Create Quadrilateral.java

---
+++ Quadrilateral.java
@@ -0,0 +1,14 @@
+public class Quadrilateral {
+ private Point a, Point b, Point c, Point d;
+ public Quadrilateral (Point a, Point b, Point c, Point d){
+   this.a = a;
+   this.b = b;
+   this.c = c;
+   this.d = d;
+ }
+  
+ public static boolean approx (double a, double b) {
+  return  
+ }
+  
+}
