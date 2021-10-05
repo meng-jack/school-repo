cdf012a425e71d3af3b441866da899234bf7fcc6
Jack Meng <jackmeng0814@gmail.com>
2021-10-05 07:55:06 -0400

    Update QuadrilteralDriver.java

--- QuadrilteralDriver.java
+++ QuadrilteralDriver.java
@@ -1,4 +1,7 @@
 public class QuadrilateralDriver {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Point x1 = new Point(1,2);
+    Point x2 = new Point(1,3);
+  }
 }
