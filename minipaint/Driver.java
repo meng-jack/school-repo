eaa4891a9aafb37f5005adfcd1bf1c148265faa9
Jack Meng <jackmeng0814@gmail.com>
2021-11-05 13:36:33 -0400

    Update Driver.java

--- minipaint/Driver.java
+++ minipaint/Driver.java
@@ -1,15 +1,19 @@
-import javax.swing.*;
-import java.awt.Color.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
 
-public class MiniPaintDriver {
+public class Main {
     public static void main(String[] args) {
-        JFrame jf = new JFrame("Jack Meng - MiniPaint");
-        MiniPaintPanel mpp = new MiniPaintPanel();
-        jf.add(mpp);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jf.pack();
-        jf.setVisible(true);
+        // for handling exceptions within the program when thrown during execution
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            JFrame jf = new JFrame("Jack Meng - MiniPaint");
+            MiniPaintPanel mpp = new MiniPaintPanel();
+            jf.add(mpp);
+            jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            jf.pack();
+            jf.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
